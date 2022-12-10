ab4385086c056db543a03730998b6994dcf207cc
phukyada <phukyada1234567@gmail.com>
2022-12-10 09:37:48 +0700

    แก้floder

--- src/mainCRUD/MainCRUD.js
+++ src/mainCRUD/MainCRUD.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 function MainCRUD() {
-  return(
+  return (
     <div>
       <p>this is Main crud</p>
-      <Link to = "/"> Back to login </Link>
+      <Link to="/"> Back to login </Link>
     </div>
-  )
+  );
 }
 
-export default MainCRUD
+export default MainCRUD;
