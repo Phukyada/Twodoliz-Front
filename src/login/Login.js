b05415472205b238c955bea8d3cd92951c490806
Sucharatana <nettyjun@gmail.com>
2022-12-11 16:19:03 +0700

    add columnnnnn

--- src/login/Login.js
+++ src/login/Login.js
@@ -9,42 +9,57 @@ import './Login.css';
 function Login() {
   return (
     <>
-    <div>
-      <div className="logo">
-        <ul> Two Du Liz </ul>
-        {/* <img src="" /> */}
-      </div>
+      <div>
+        <div class="row">
+          <div class="column"
+            style={{
+              backgroundColor: 'green',
+            }}
+          >
+            <div className="logo">
+              <ul> Two Du Liz </ul>
+              {/* <img src="" /> */}
+            </div>
+          </div>
+          <div class="column"
+            style={{
+              backgroundColor: 'blue',
+            }}
+          >
+            <div className="username">
+              <ul> Username </ul>
+              <input type="" />
+            </div>
 
-      <div className="username">
-        <ul> Username </ul>
-        <input type=""/>
-      </div>
+            <div className="password">
+              <ul> Password </ul>
+              <input type="password" />
+            </div>
 
-      <div className="password">
-        <ul> Password </ul>
-        <input type="password"/>
-      </div>
-      
-      <div className="login-bt">
-        <button><a href="/"> Log in </a></button>
-      </div>
-      
-      <div className="signup-bt">
-      <div>Don't have an account yet? </div>
-      <button><a href="/register"> Sign up now! </a></button>
-      
-      
+            <div className="login-bt">
+              <button><a href="/"> Log in </a></button>
+            </div>
 
+            <div className="signup-bt">
+              <div>Don't have an account yet? </div>
+              <button><a href="/register"> Sign up now! </a></button>
+            </div>
+          </div>
+
+
+
+
+
+        </div>
       </div>
-    </div>
     </>
   )
-  
-  }
-  
-  
-  
-  
+
+}
+
+
+
+
 
 
 
