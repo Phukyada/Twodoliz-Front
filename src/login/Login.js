9825625c860ef28f1d1f4ddec3fe7d36639e8b09
J.Phaiboon <phaiboon760@gmail.com>
2022-12-11 16:37:11 +0700

    add columnn with ant design

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Col, Row } from 'antd';
 // import "./App.css";
 // import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './Login.css';
@@ -9,49 +10,37 @@ import './Login.css';
 function Login() {
   return (
     <>
-      <div>
-        <div class="row">
-          <div class="column"
-            style={{
-              backgroundColor: 'green',
-            }}
-          >
+      <Row>
+        <Col span={12} className="col-1">
+          <div>
             <div className="logo">
               <ul> Two Du Liz </ul>
               {/* <img src="" /> */}
             </div>
           </div>
-          <div class="column"
-            style={{
-              backgroundColor: 'blue',
-            }}
-          >
-            <div className="username">
-              <ul> Username </ul>
-              <input type="" />
-            </div>
-
-            <div className="password">
-              <ul> Password </ul>
-              <input type="password" />
-            </div>
-
-            <div className="login-bt">
-              <button><a href="/"> Log in </a></button>
-            </div>
-
-            <div className="signup-bt">
-              <div>Don't have an account yet? </div>
-              <button><a href="/register"> Sign up now! </a></button>
-            </div>
+        </Col>
+        <Col span={12} className="col-2">
+          <div className="username">
+            <ul> Username </ul>
+            <input type="" />
           </div>
 
+          <div className="password">
+            <ul> Password </ul>
+            <input type="password" />
+          </div>
 
+          <div className="login-bt">
+            <button><a href="/"> Log in </a></button>
+          </div>
 
+          <div className="signup-bt">
+            <div>Don't have an account yet? </div>
+            <button><a href="/register"> Sign up now! </a></button>
+          </div>
+        </Col>
+      </Row>
 
-
-        </div>
-      </div>
     </>
   )
 
