a6df3b4f401d147a662325170c8b1370c6546596
J.Phaiboon <phaiboon760@gmail.com>
2022-12-12 17:58:38 +0700

    login and register

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,19 +1,47 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { Col, Row } from 'antd';
+// import "./App.css";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './login.css';
+// import Register from "./register/Register";
+// import MainCRUD from "./mainCRUD/MainCRUD";
 
 
 function Login() {
-  let navigate = useNavigate();
-
   return (
-    <div>
-      <p>this is login</p>
-      <button onClick={() => navigate('/home')}>
-        main crud
-      </button>
-    </div>
-  );
-  
-}
+    <>
+      <Row>
+        <Col span={12} className="col-1">
+          <div>
+            <div className="logo">
+              <img src="https://i.im.ge/2022/12/11/dqez5y.logoo.png" border="0"/>
+            </div>
+          </div>
+        </Col>
+        <Col span={12} className="col-2">
+          <div className="username">
+            <ul> Username </ul>
+            <input type="" className="userinput"/>
+          </div>
 
+          <div className="password">
+            <ul> Password </ul>
+            <input type="password" className="passwordinput"/>
+          </div>
+
+          <div className="login-bt">
+            <button class="button1"><a href="/home"> Log in </a></button>
+          </div>
+
+          <div className="signup-bt">
+            Don't have an account yet?
+            <button class="button2"><a href="/register"> Sign up now! </a></button>
+          </div>
+        </Col>
+      </Row>
+
+    </>
+  )
+
+}
 export default Login;
--- src/register/Register.js
+++ src/register/Register.js
@@ -1,13 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "./register.css"
 
-function Register() {
+
+const Register = () => {
+
+
   return (
-    <div>
-      <p>this is regester</p>
-      <Link to="/"> Back to login </Link>
+    <div className="register">
+      <div className="justify-content-end">
+        <div className="header">
+          <img src="https://i.im.ge/2022/12/12/dipNnW.logo2dliz.png" className="logo" />
+        </div>
+        <div className="input">
+          <div className="al">
+            <ul> Username </ul>
+          </div>
+          <input type="" className="boxInput" />
+
+          <div className="al">
+            <ul> E-mail </ul>
+          </div>
+          <input type="" className="boxInput" />
+
+          <div className="al">
+            <ul> Password </ul>
+          </div>
+          <input type="password" className="boxInput" />
+        </div>
+
+        <button class="signup"><a href="/" >Create Account</a></button>
+
+        <div className="login">
+          Already have an account?
+          <button class="logbut"><a href="/"> Login</a></button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default Register;
