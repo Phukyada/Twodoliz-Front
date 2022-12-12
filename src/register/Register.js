80eac1871baf56b929b99d7162881df4acf494c8
Sucharatana <nettyjun@gmail.com>
2022-12-12 17:02:14 +0700

    register yay

--- src/register/Register.js
+++ src/register/Register.js
@@ -1,44 +1,46 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./register.css"
 
 
-  const Register = () => {
+const Register = () => {
 
 
   return (
     <div className="register">
-      
-      <div className="header">
-        <img src="https://i.im.ge/2022/12/12/dipNnW.logo2dliz.png" />
+      <div className="justify-content-end">
+        <div className="header">
+          <img src="https://i.im.ge/2022/12/12/dipNnW.logo2dliz.png" className="logo" />
+        </div>
+        <div className="input">
+          <div className="al">
+            <ul> Username </ul>
+          </div>
+          <input type="" className="boxInput" />
+
+          <div className="al">
+            <ul> E-mail </ul>
+          </div>
+          <input type="" className="boxInput" />
+
+          <div className="al">
+            <ul> Password </ul>
+          </div>
+          <input type="password" className="boxInput" />
+        </div>
+
+        <button class="signup"><a href="/" >Create Account</a></button>
+
+        <div className="login">
+          Already have an account?
+          <button class="logbut"><a href="/login"> Login</a></button>
+        </div>
+
+
+
+
       </div>
-      
-      <div className = "username">
-        <ul> Username </ul>
-        <input type="" className="userInput"/>
-      </div>
-      
-      <div className = "email">
-        <ul> E-mail </ul>
-        <input type="" className="emailInput"/>
-      </div> 
-
-      <div className = "password">
-        <ul> Password </ul>
-        <input type="password" className="passwordInput"/>
-      </div>      
-      
-
-     <button class="signup"><a href="/" >Create Account</a></button>
-
-
-     <div className="loginbt">
-      Already have an account?
-      <button class="logbut"><a href="/login"> Login</a></button>
-     </div>
-    
     </div>
   )
 }
 
 export default Register;
-  
