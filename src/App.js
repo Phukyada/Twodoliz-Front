cc55741c5211e9d1d9a369699735000cdf3312bf
sircsp <98664705+sircsp@users.noreply.github.com>
2022-12-12 13:53:28 +0700

    register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./login/Login";
 import Register from "./register/Register";
 import MainCRUD from "./mainCRUD/MainCRUD";
@@ -7,14 +7,7 @@ import MainCRUD from "./mainCRUD/MainCRUD";
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <h1> helllo</h1>
-      </header>
-      <nav>
-        <NavLink to="/"> login</NavLink>
-        <NavLink to="/register">register</NavLink>
-        <NavLink to="/mainCRUD">mainCRUD</NavLink>
-      </nav>
+      
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
--- src/register/Register.js
+++ src/register/Register.js
@@ -1,13 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState} from "react";
+import "./register.css"
 
-function Register() {
+
+  const Register = () => {
+
+
   return (
-    <div>
-      <p>this is regester</p>
-      <Link to="/"> Back to login </Link>
+    <div className="register">
+      
+      <div className="header">
+        <img src="https://i.im.ge/2022/12/12/d7PZHh.TwoDuLiz.png" />
+      </div>
+      
+      <div className = "username">
+        <ul> Username </ul>
+        <input type="" className="userInput"/>
+      </div>
+      
+      <div className = "email">
+        <ul> E-mail </ul>
+        <input type="" className="emailInput"/>
+      </div> 
+
+      <div className = "password">
+        <ul> Password </ul>
+        <input type="password" className="passwordInput"/>
+      </div>      
+      
+
+     <button class="signup"><a href="/" >Create Account</a></button>
+
+
+     <div className="loginbt">
+      Already have an account?
+      <button class="logbut"><a href="/login"> Login</a></button>
+     </div>
+    
     </div>
-  );
+  )
 }
 
 export default Register;
+  
